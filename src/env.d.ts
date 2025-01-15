41599364c3aa9ccb6aa0a3389853833a69f1c0d5
RikiRilis <thewhitzip@gmail.com>
2025-01-14 23:23:39 -0400

    Basic dependencies added

---
+++ src/env.d.ts
@@ -0,0 +1,25 @@
+/// <reference types="astro/client" />
+
+import type { ToastOptions } from './libs/toast'
+
+interface Window {
+	getThemePreference(): 'dark' | 'light'
+	toast({ ToastOptions }: ToastOptions): void
+}
+
+// interface ImportMetaEnv {
+// 	readonly EMAILJS_KEY: string
+// 	readonly EMAILJS_SERVICE_ID: string
+// 	readonly EMAILJS_TEMPLATE_ID: string
+// }
+
+// interface ImportMeta {
+// 	readonly env: ImportMetaEnv
+// }
+
+declare global {
+	interface Window {
+		getThemePreference: function
+		toast: function
+	}
+}

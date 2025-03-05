3749a1c5d8b8cb62e7f0abd4617fc9152524b327
RikiRilis <thewhitzip@gmail.com>
2025-03-05 00:07:39 -0400

    Philosohpy added

---
+++ src/interfaces/philosophy.ts
@@ -0,0 +1,5 @@
+export interface Philosophy {
+	title: string
+	description: string
+	icon: string
+}
--- src/libs/consts.ts
+++ src/libs/consts.ts
@@ -1,5 +1,6 @@
 import type { NavItem } from '@/interfaces/navItems'
 import { getI18N } from '@/languages/index.ts'
+import type { Philosophy } from '../interfaces/philosophy'
 
 export const navItemsProvider = (currentLocale: string | undefined): NavItem[] => {
 	const i18n = getI18N({ currentLocale })
@@ -30,3 +31,28 @@ export const navItemsProvider = (currentLocale: string | undefined): NavItem[] =
 export const businessEmail = 'fotoestudioelchevere@hotmail.com'
 export const businessInstagram = 'https://instagram.com/fotoestudioelchevere/'
 export const businessWhatsApp = 'https://wa.me/18095734173'
+
+export const philosophy = (currentLocale: string | undefined): Philosophy[] => {
+	const i18n = getI18N({ currentLocale })
+
+	return [
+		{
+			title: i18n.OUR_MISSION,
+			description:
+				'Empowering individuals and businesses with innovative solutions that drive growth, efficiency, and success.',
+			icon: '🚀',
+		},
+		{
+			title: i18n.OUR_VISION,
+			description:
+				'To be the leading force in technological advancements, shaping a better future for the global community.',
+			icon: '🌍',
+		},
+		{
+			title: i18n.OUR_VALUES,
+			description:
+				'Integrity, Innovation, and Excellence—these principles guide our every decision and action.',
+			icon: '💡',
+		},
+	]
+}

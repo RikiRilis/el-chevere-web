06f7dc49d9a92f4fdd357f0bb731c3d67924af1c
RikiRilis <thewhitzip@gmail.com>
2025-03-07 14:59:52 -0400

    Question modified

--- src/services/faqs.ts
+++ src/services/faqs.ts
@@ -11,7 +11,7 @@ export const getFaqs = (currentLocale: string | undefined): Faq[] => {
 				'Las fotos digitales, tomadas antes de las 5:00 PM, se les entregarán al cliente en el trascurso del mismo día, dentro del horario laboral: 8:00 AM - 7:00 PM.',
 				'Las fotos de tiempo, tomadas en el horario laboral: 8:00 AM - 7:00 PM, se les entregarán al cliente de manera física, con la marca de agua del estiudio, en un plazo de 2 a 3 días, dependiendo de la complicidad de las fotos seleccionadas.',
 			],
-			question: '¿Cuáles diferencias existen entre el proceso "digital" y el proceso "de tiempo?"',
+			question: '¿Cuáles diferencias existen entre el proceso "digital" y el proceso "de tiempo"?',
 		},
 		{
 			answers: ['A'],

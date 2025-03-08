1f886814645d71cf91febe4f35e271bc1fe533b1
RikiRilis <thewhitzip@gmail.com>
2025-03-08 15:39:21 -0400

    Env variables added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel';
@@ -8,7 +8,6 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
@@ -16,19 +15,39 @@ export default defineConfig({
       prefixDefaultLocale: false
     },
   },
-
   output: 'server',
-
   build: {
         inlineStylesheets: 'always',
     },
-
   compressHTML: true,
   prefetch: true,
 
   devToolbar: {
       enabled: false,
   },
-
   adapter: vercel(),
+  env: {
+    schema: {
+      SUPABASE_URL: envField.string({
+        context: 'server',
+        access: 'secret'
+      }),
+      SUPABASE_ANON_KEY: envField.string({
+        context: 'server',
+        access: 'secret'
+      }),
+      EMAILJS_KEY: envField.string({
+        context: 'server',
+        access: 'secret'
+      }),
+      EMAILJS_SERVICE_ID: envField.string({
+        context: 'server',
+        access: 'secret'
+      }),
+      EMAILJS_TEMPLATE_ID: envField.string({
+        context: 'server',
+        access: 'public'
+      })
+    }
+  }
 });

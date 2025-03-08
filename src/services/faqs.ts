b5a5f54a5202b8d68821a98a5340b4658b0831b5
RikiRilis <thewhitzip@gmail.com>
2025-03-07 23:29:10 -0400

    FAQ section implemented

--- src/services/faqs.ts
+++ src/services/faqs.ts
@@ -6,32 +6,28 @@ export const getFaqs = (currentLocale: string | undefined): Faq[] => {
 
 	return [
 		{
-			answers: [
-				'La principal diferencia radica en la fecha que se le entregan las fotos.',
-				'Las fotos digitales, tomadas antes de las 5:00 PM, se les entregarán al cliente en el trascurso del mismo día, dentro del horario laboral: 8:00 AM - 7:00 PM.',
-				'Las fotos de tiempo, tomadas en el horario laboral: 8:00 AM - 7:00 PM, se les entregarán al cliente de manera física, con la marca de agua del estiudio, en un plazo de 2 a 3 días, dependiendo de la complicidad de las fotos seleccionadas.',
-			],
-			question: '¿Cuáles diferencias existen entre el proceso "digital" y el proceso "de tiempo"?',
+			answers: [i18n.FAQ_QA_1, i18n.FAQ_QA_2, i18n.FAQ_QA_3],
+			question: i18n.FAQ_Q_1,
 		},
 		{
-			answers: ['A'],
-			question: 'asd',
+			answers: [i18n.FAQ_QA_4],
+			question: i18n.FAQ_Q_2,
 		},
 		{
-			answers: ['A'],
-			question: 'asd',
+			answers: [i18n.FAQ_QA_5],
+			question: i18n.FAQ_Q_3,
 		},
 		{
-			answers: ['A'],
-			question: 'asd',
+			answers: [i18n.FAQ_QA_6],
+			question: i18n.FAQ_Q_4,
 		},
 		{
-			answers: ['A'],
-			question: 'asd',
+			answers: [i18n.FAQ_QA_7],
+			question: i18n.FAQ_Q_5,
 		},
 		{
-			answers: ['A'],
-			question: 'asd',
+			answers: [i18n.FAQ_QA_8],
+			question: i18n.FAQ_Q_6,
 		},
 	]
 }

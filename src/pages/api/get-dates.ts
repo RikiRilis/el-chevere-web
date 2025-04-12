56ddb643ad6a2cd90ddfe6b4b6e64d4a5d58e57a
RikiRilis <thewhitzip@gmail.com>
2025-04-12 01:05:47 -0400

    Add conditionals for body in get API

--- src/pages/api/get-dates.ts
+++ src/pages/api/get-dates.ts
@@ -1,6 +1,9 @@
 import { supabase } from '@/db/supabase'
 
 interface RequestBody {
+	limit: number
+	from: number
+	to: number
 	column: string
 	value: string
 	column2: string
@@ -8,17 +11,34 @@ interface RequestBody {
 }
 
 export async function POST({ request }: { request: Request }) {
-	const { column, value, column2, value2 }: RequestBody = await request.json()
+	const { limit, from, to, column, value, column2, value2 }: RequestBody = await request.json()
 
-	const { data, error } = await supabase
+	let query = supabase
 		.from('dates')
-		.select('*')
-		.eq(column, value)
-		.eq(column2, value2)
+		.select('*', { count: 'exact' })
+		.order('timestamp', { ascending: false })
+
+	if (column && value) {
+		query = query.eq(column, value)
+	}
+
+	if (column2 && value2) {
+		query = query.eq(column2, value2)
+	}
+
+	if (from !== undefined && to !== undefined) {
+		query = query.range(from, to)
+	}
+
+	if (limit) {
+		query = query.limit(limit)
+	}
+
+	const { data, count, error } = await query
 
 	if (error) {
 		return new Response(JSON.stringify({ error }), { status: 500 })
 	}
 
-	return new Response(JSON.stringify({ data }), { status: 200 })
+	return new Response(JSON.stringify({ data, count }), { status: 200 })
 }

9f97a90e935afb8168d4da3ac323c9a636001941
RikiRilis <thewhitzip@gmail.com>
2025-07-10 23:15:59 -0400

    Different parallax image for day

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,6 +16,12 @@ export default {
 			},
 			backgroundImage: {
 				'parallax-1': "url('/statics/parallax-1.webp')",
+				'parallax-2': "url('/statics/parallax-2.webp')",
+				'parallax-3': "url('/statics/parallax-3.webp')",
+				'parallax-4': "url('/statics/parallax-4.webp')",
+				'parallax-5': "url('/statics/parallax-5.webp')",
+				'parallax-6': "url('/statics/parallax-6.webp')",
+				'parallax-7': "url('/statics/parallax-7.webp')",
 			},
 		},
 	},
